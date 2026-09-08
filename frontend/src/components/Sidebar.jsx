2ce569f5daca63ddc31297acaef5db6acdf8921e
Divye <peepladivye@github.com>
2026-09-08 12:15:49 +0530

    auto: sync changes

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { api } from '../lib/api';
+import LanguageSwitcher from './LanguageSwitcher';
 
 // Icon components for navigation
 const ChevronRightIcon = () => (
